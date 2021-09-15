6f666af6aa26c42bea04bef66a10f6571082aaa8
lucas__almeida <lucaslap27@gmail.com>
2021-09-15 14:40:18 -0400

    Traz url para download da musica que foi passada

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+import fs from "fs";
+import axios from "axios";
 
 function App() {
+  const [link, setLink] = useState("");
+  const [urlVideo, setUrlVideo] = useState(null);
+  const [nameVideo, setNameVideo] = useState(null);
+  const [status, setStatus] = useState(null);
+
+  const getVideo = async () => {
+    let idVideo = link.split('v=')[1].split('&')[0]
+    var options = {
+      method: 'GET',
+      url: 'https://youtube-mp36.p.rapidapi.com/dl',
+      params: {id: idVideo},
+      headers: {
+        'x-rapidapi-host': 'youtube-mp36.p.rapidapi.com',
+        'x-rapidapi-key': '7ef0023ea0mshe5462d94f5d7854p188505jsn9267404dc9cd'
+      }
+    };
+    let rep = await axios.request(options)
+    setStatus(rep.data.status)
+    setUrlVideo(rep.data.link)
+    setNameVideo(rep.data.title)
+    setLink('')
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Baixar Musica do <b>YouTube</b>
+      </h1>
+      <input
+        className="inputLink"
+        type="text"
+        onChange={(t) => setLink(t.target.value)}
+        placeholder="Informe o link do video"
+        value={link}
+      />
+      <button className="btnBaixar" onClick={() => getVideo()}>
+        Procurar
+      </button>
+      { status === 'ok' ? (
+        <div>
+          <h1>{nameVideo}</h1>
+          <a href={urlVideo} className="btnDownload" download>Baixar</a>
+        </div>
+      ) : null }
     </div>
   );
 }
