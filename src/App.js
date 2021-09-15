1ce3834a0438ea02bb2b64e77a10a6a5dd59ea9c
lucas__almeida <lucaslap27@gmail.com>
2021-09-15 15:56:06 -0400

    Ajusta layout de botão de baixar musica

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import "./App.css";
-
-import fs from "fs";
 import axios from "axios";
 
 function App() {
@@ -10,23 +8,24 @@ function App() {
   const [nameVideo, setNameVideo] = useState(null);
   const [status, setStatus] = useState(null);
 
-  const getVideo = async () => {
-    let idVideo = link.split('v=')[1].split('&')[0]
+  const getAudio = async () => {
+    let idVideo = link.split("v=")[1].split("&")[0];
     var options = {
-      method: 'GET',
-      url: 'https://youtube-mp36.p.rapidapi.com/dl',
-      params: {id: idVideo},
+      method: "GET",
+      url: "https://youtube-mp36.p.rapidapi.com/dl",
+      params: { id: idVideo },
       headers: {
-        'x-rapidapi-host': 'youtube-mp36.p.rapidapi.com',
-        'x-rapidapi-key': '7ef0023ea0mshe5462d94f5d7854p188505jsn9267404dc9cd'
-      }
+        "x-rapidapi-host": "youtube-mp36.p.rapidapi.com",
+        "x-rapidapi-key": "7ef0023ea0mshe5462d94f5d7854p188505jsn9267404dc9cd",
+      },
     };
-    let rep = await axios.request(options)
-    setStatus(rep.data.status)
-    setUrlVideo(rep.data.link)
-    setNameVideo(rep.data.title)
-    setLink('')
+    let rep = await axios.request(options);
+    setStatus(rep.data.status);
+    setUrlVideo(rep.data.link);
+    setNameVideo(rep.data.title);
+    setLink("");
   };
+
   return (
     <div className="App">
       <h1>
@@ -39,15 +38,17 @@ function App() {
         placeholder="Informe o link do video"
         value={link}
       />
-      <button className="btnBaixar" onClick={() => getVideo()}>
+      <button className="btnBaixar" onClick={() => getAudio()}>
         Procurar
       </button>
-      { status === 'ok' ? (
+      {status === "ok" ? (
         <div>
           <h1>{nameVideo}</h1>
-          <a href={urlVideo} className="btnDownload" download>Baixar</a>
+          <a href={urlVideo} className="btnDownload" download>
+            Baixar
+          </a>
         </div>
-      ) : null }
+      ) : null}
     </div>
   );
 }
